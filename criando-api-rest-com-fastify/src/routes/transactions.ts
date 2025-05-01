ab551c29ac483973154dcc258e963d60533a03f5
michael wellington lopes <michael23.wellington@gmail.com>
2025-05-01 16:15:18 -0300

    (Project 2 - Implementation Routes): Listing transactions

--- criando-api-rest-com-fastify/src/routes/transactions.ts
+++ criando-api-rest-com-fastify/src/routes/transactions.ts
@@ -11,6 +11,20 @@ export async function transactionsRoutes(app: FastifyInstance) {
     };
   });
 
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions').where({ id }).first();
+
+    return {
+      transaction,
+    };
+  });
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
