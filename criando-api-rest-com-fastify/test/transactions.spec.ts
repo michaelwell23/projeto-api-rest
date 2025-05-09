fe22183509f7b6ba04cbca4e382c00cf2b4d2faa
michael wellington lopes <michael23.wellington@gmail.com>
2025-05-09 14:58:26 -0300

    Project 2 - Automated Testing: create test list transactions

--- criando-api-rest-com-fastify/test/transactions.spec.ts
+++ criando-api-rest-com-fastify/test/transactions.spec.ts
@@ -13,12 +13,37 @@ describe('Transactions routes', () => {
   });
 
   it('Should be able to creante a new transaction', async () => {
-    const response = await request(app.server).post('/transactions').send({
-      title: 'New Transaction',
-      amount: 50000,
-      type: 'credit',
-    });
+    const response = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 50000,
+        type: 'credit',
+      })
+      .expect(201);
+  });
 
-    expect(response.statusCode).toEqual(201);
+  it('Should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 50000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 50000,
+      }),
+    ]);
   });
 });
