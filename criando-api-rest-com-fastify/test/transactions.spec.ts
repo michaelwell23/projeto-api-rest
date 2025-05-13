21916157a448b44fb71d03e64597f2faaa6d1563
michael wellington lopes <michael23.wellington@gmail.com>
2025-05-13 12:53:45 -0300

    Project 2 - Automated Testing: finalizing application tests

--- criando-api-rest-com-fastify/test/transactions.spec.ts
+++ criando-api-rest-com-fastify/test/transactions.spec.ts
@@ -52,4 +52,65 @@ describe('Transactions routes', () => {
       }),
     ]);
   });
+
+  it('Should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 50000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies || [])
+      .expect(200);
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id;
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies || [])
+      .expect(200);
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 50000,
+      })
+    );
+  });
+
+  it('Should be able to get a summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 50000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies || [])
+      .send({
+        title: 'New Transaction',
+        amount: 20000,
+        type: 'debit',
+      });
+
+    const summaryResponse = await request(app.server)
+      .get(`/transactions/summary`)
+      .set('Cookie', cookies || [])
+      .expect(200);
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 30000,
+    });
+  });
 });
