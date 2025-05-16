24cb11f06073f69eeb4ca63da504c16e3c8b22cf
michael wellington lopes <michael23.wellington@gmail.com>
2025-05-16 13:56:09 -0300

    Project 03 API with SOLID/Project Structure: Loading environment variables and configuring ESLint

--- 03-api-utilizando-solid/src/server.ts
+++ 03-api-utilizando-solid/src/server.ts
@@ -1,5 +1,6 @@
 import { app } from './app';
+import { env } from './env';
 
-app.listen({ host: '0.0.0.0', port: 3000 }).then(() => {
-  console.log(`Server is running on http://localhost:3000`);
+app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
+  console.log(`Server is running on http://localhost:${env.PORT}`);
 });
