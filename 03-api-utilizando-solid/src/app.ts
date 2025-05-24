7a47e0b9b154c1aa0328e8193b8f80a049dd5c5c
michael wellington lopes <michael23.wellington@gmail.com>
2025-05-24 16:11:15 -0300

    Project 03 API with SOLID/Prisma ORM Integration - Create Register Controller

--- 03-api-utilizando-solid/src/app.ts
+++ 03-api-utilizando-solid/src/app.ts
@@ -1,25 +1,6 @@
 import fastify from 'fastify';
-import { z } from 'zod';
-import { prisma } from './lib/prisma';
+import { appRoutes } from './http/routes';
 
 export const app = fastify();
 
-app.post('/users', async (resquest, reply) => {
-  const resgisterBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
-
-  const { name, email, password } = resgisterBodySchema.parse(resquest.body);
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
-
-  return reply.status(201).send({ Messagem: 'User created' });
-});
+app.register(appRoutes);
