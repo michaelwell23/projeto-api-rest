b53d943d769e30d1e0e0f8f8310099f193b8aab6
michael wellington lopes <michael23.wellington@gmail.com>
2025-05-26 14:44:53 -0300

    Project 03 API with SOLID/Use Case and Design Patterns - Password hash and validation

--- 03-api-utilizando-solid/src/http/controllers/register.ts
+++ 03-api-utilizando-solid/src/http/controllers/register.ts
@@ -1,5 +1,5 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-
+import { hash } from 'bcryptjs';
 import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
 
@@ -12,11 +12,22 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = resgisterBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 8);
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send('Email already exist.');
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   });
 
