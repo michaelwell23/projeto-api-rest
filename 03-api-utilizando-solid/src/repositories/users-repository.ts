820b0e3f58bd6fd5708a6ff32b1d948f9c23d2ae
michael wellington lopes <michael23.wellington@gmail.com>
2025-06-09 11:13:37 -0300

    Project 03 API with SOLID/Implementations UseCase - Authenticate UseCase & Create Test and Authenticate Controller

--- 03-api-utilizando-solid/src/repositories/users-repository.ts
+++ 03-api-utilizando-solid/src/repositories/users-repository.ts
@@ -2,16 +2,6 @@ import { Prisma, User } from '@prisma/client';
 
 export interface UsersRepository {
   create(data: Prisma.UserCreateInput): Promise<User>;
-  findByEmail(
-    email: string
-  ): Promise<{ id: string; name: string; email: string } | null>;
-  // findById(
-  //   id: string
-  // ): Promise<{ id: string; name: string; email: string } | null>;
-  // update(
-  //   id: string,
-  //   data: { name?: string; email?: string; password_hash?: string }
-  // ): Promise<void>;
-  // delete(id: string): Promise<void>;
-  // list(): Promise<Array<{ id: string; name: string; email: string }>>;
+  findByEmail(email: string): Promise<User | null>;
+  // findById(id: string): Promise<User | null>;
 }
