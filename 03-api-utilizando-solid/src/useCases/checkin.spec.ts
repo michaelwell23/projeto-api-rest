8c65397d2110b09e25ba20d46b62268bf99e5f1e
michael wellington lopes <michael23.wellington@gmail.com>
2025-06-16 14:28:16 -0300

    Project 03 API with SOLID/Implementations UseCase - Validating the distance between the user's check-in and the gym

--- 03-api-utilizando-solid/src/useCases/checkin.spec.ts
+++ 03-api-utilizando-solid/src/useCases/checkin.spec.ts
@@ -3,6 +3,7 @@ import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest';
 import { CheckInUseCase } from './checkin';
 import { GymsRepositoryInMemory } from '@/repositories/in-memory/in-memory-gyms-respository';
 import { Decimal } from '@prisma/client/runtime';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let gymsRepositoryInMemory: GymsRepositoryInMemory;
@@ -15,12 +16,12 @@ describe('Check-in Use Case', () => {
     sut = new CheckInUseCase(checkInsRepository, gymsRepositoryInMemory);
 
     await gymsRepositoryInMemory.items.push({
-      id: 'gym-1',
-      title: 'Gym 1',
+      id: 'gym-01',
+      title: 'Gym-01',
       description: 'Description of Gym 1',
       phone: '123456789',
-      latitude: new Decimal(-23.5563277),
-      longitude: new Decimal(-46.6454528),
+      latitude: new Decimal(-23.5505),
+      longitude: new Decimal(-46.6333),
     });
 
     vi.useRealTimers();
@@ -32,8 +33,8 @@ describe('Check-in Use Case', () => {
 
   it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
-      userId: 'user-1',
-      gymId: 'gym-1',
+      userId: 'user-01',
+      gymId: 'gym-01',
       userLatitude: -23.5505,
       userLongitude: -46.6333,
     });
@@ -65,8 +66,8 @@ describe('Check-in Use Case', () => {
     vi.setSystemTime(new Date(2023, 3, 1, 10, 0, 0));
 
     await sut.execute({
-      userId: 'user-1',
-      gymId: 'gym-1',
+      userId: 'user-01',
+      gymId: 'gym-01',
       userLatitude: -23.5505,
       userLongitude: -46.6333,
     });
@@ -74,12 +75,32 @@ describe('Check-in Use Case', () => {
     vi.setSystemTime(new Date(2023, 3, 2, 10, 0, 0));
 
     const { checkIn } = await sut.execute({
-      userId: 'user-1',
-      gymId: 'gym-1',
+      userId: 'user-01',
+      gymId: 'gym-01',
       userLatitude: -23.5505,
       userLongitude: -46.6333,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to check in on distant gym', async () => {
+    await gymsRepositoryInMemory.items.push({
+      id: 'gym-02',
+      title: 'Gym-02',
+      description: 'Description of Gym 2',
+      phone: '967809788',
+      latitude: new Decimal(-23.1948993),
+      longitude: new Decimal(-47.3310689),
+    });
+
+    await expect(() =>
+      sut.execute({
+        userId: 'user-01',
+        gymId: 'gym-02',
+        userLatitude: -23.5505,
+        userLongitude: -46.6333,
+      })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
