a433903869638c686f06ff22c74cb5aa04cf4c12
michael wellington lopes <michael23.wellington@gmail.com>
2025-06-28 13:22:03 -0300

    Project 03 API with SOLID/Controllers & Test E2E - Create Profile controller

--- 03-api-utilizando-solid/src/http/controllers/profile.ts
+++ 03-api-utilizando-solid/src/http/controllers/profile.ts
@@ -1,9 +1,12 @@
+import { makeGetUserProfileUseCase } from '@/useCases/factories/make-get-user-profile-use.case';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify();
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  console.log(request.user);
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
 
-  return reply.status(201).send({ Messagem: 'User is logged ' });
+  return reply.status(200).send({ user });
 }
