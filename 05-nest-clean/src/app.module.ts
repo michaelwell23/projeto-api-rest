29e0b177b8d471ce53632756a8925d9f2989553f
michael wellington lopes <michael23.wellington@gmail.com>
2025-07-19 14:06:59 -0300

    Project 05 - NestJS / Nest Fundamentals - Modules, Services and Controller & Configuring ESlint and Prettier

--- 05-nest-clean/src/app.module.ts
+++ 05-nest-clean/src/app.module.ts
@@ -5,6 +5,6 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], //providers é tudo aquilo que não recebe requisições HTTP diretamente. É tudo que é injetável, como serviços, repositórios, etc.
 })
 export class AppModule {}
