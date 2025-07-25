3ca5ba31c78c2457350a6a95fd59b72968e31fdd
michael wellington lopes <michael23.wellington@gmail.com>
2025-07-25 14:13:20 -0300

    Project 05 - NestJS / Nest Fundamentals - Using Rest Client extension in VSCode

--- 05-nest-clean/src/app.module.ts
+++ 05-nest-clean/src/app.module.ts
@@ -4,7 +4,7 @@ import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 
 @Module({
-  imports: [CreateAccountController],
+  controllers: [CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
