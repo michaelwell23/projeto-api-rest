36aed923c8d4c8375ab792d214e305e08667eb1f
michael wellington lopes <michael23.wellington@gmail.com>
2025-07-26 16:40:38 -0300

    Project 05 - NestJS / Nest Fundamentals - Using ConfigModule on Nest

--- 05-nest-clean/src/main.ts
+++ 05-nest-clean/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService);
+  const port = configService.get('PORT', { infer: true }) || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
