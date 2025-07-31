ccf7a8415a88ee59b823fb522b46cf6afd10a836
michael wellington lopes <michael23.wellington@gmail.com>
2025-07-31 15:17:30 -0300

    Project 05 - NestJS / Nest Fundamentals - Creating authentication decorator and question creation controller

--- 05-nest-clean/src/controllers/create-question.controller.ts
+++ 05-nest-clean/src/controllers/create-question.controller.ts
@@ -1,16 +1,53 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { CurrentUser } from 'src/auth/current-user-decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserPayload } from 'src/auth/jwt.strategy';
+import { ZodValidationPipe } from 'src/pipes/zod-validate-pipe';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prismaService: PrismaService) {}
 
   @Post()
-  async handle(@Req() request: Request) {
-    console.log(request.user);
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload
+  ) {
+    const { title, content } = body;
+    const userId = user.sub;
+
+    const slug = this.convertToSlug(title);
+
+    await this.prismaService.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    });
 
     return 'ok';
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/^-|-$/g, '')
+      .replace(/-+/g, '-');
   }
 }
