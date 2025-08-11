c330d6dd1b624a2d2e698f7cba3ef4da33401812
michael wellington lopes <michael23.wellington@gmail.com>
2025-08-11 14:36:02 -0300

    Project 05 - NestJS / Decoupling Nestjs layers - Talking Between Layers & Creating Prisma Schema

--- 05-nest-clean/src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ 05-nest-clean/src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -1,12 +1,24 @@
+import { PrismaService } from './../prisma.service';
 import { PaginationParams } from '@/core/repositories/pagination-params';
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { Injectable } from '@nestjs/common';
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper';
 
 @Injectable()
 export class PrismaQuestionRespository implements QuestionsRepository {
-  findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.');
+  constructor(private prismaService: PrismaService) {}
+
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prismaService.question.findUnique({
+      where: { id },
+    });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionMapper.toDomain(question);
   }
 
   findBySlug(slug: string): Promise<Question | null> {
