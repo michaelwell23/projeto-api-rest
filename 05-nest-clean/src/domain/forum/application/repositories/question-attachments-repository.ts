28d0bc04182377a8f47c7054102763f5966b72df
michael wellington lopes <michael23.wellington@gmail.com>
2025-10-13 12:50:51 -0300

    Project 05 - NestJS /Upload and Relationships - fix

--- 05-nest-clean/src/domain/forum/application/repositories/question-attachments-repository.ts
+++ 05-nest-clean/src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -1,6 +1,7 @@
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
 
 export abstract class QuestionAttachmentsRepository {
+  items: any;
   abstract findManyByQuestionId(
     questionId: string
   ): Promise<QuestionAttachment[]>;
