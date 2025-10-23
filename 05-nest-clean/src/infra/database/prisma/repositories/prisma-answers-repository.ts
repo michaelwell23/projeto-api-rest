a6afc2c9caa6ef54f992caaa42c4f89497e20ff7
michael wellington lopes <michael23.wellington@gmail.com>
2025-10-23 10:00:00 -0300

    responses with attachments

--- 05-nest-clean/src/infra/database/prisma/repositories/prisma-answers-repository.ts
+++ 05-nest-clean/src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -1,22 +1,24 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository';
 import { Answer } from '@/domain/forum/enterprise/entities/answer';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { PrismaAnswerMapper } from '../mappers/prisma-answer-mapper';
-import { PaginationParams } from '@/core/repositories/pagination-params';
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository';
 import { DomainEvents } from '@/core/events/domain-events';
 
 @Injectable()
 export class PrismaAnswersRepository implements AnswersRepository {
   constructor(
-    private prismaService: PrismaService,
+    private prisma: PrismaService,
     private answerAttachmentsRepository: AnswerAttachmentsRepository
   ) {}
 
   async findById(id: string): Promise<Answer | null> {
-    const answer = await this.prismaService.answer.findUnique({
-      where: { id },
+    const answer = await this.prisma.answer.findUnique({
+      where: {
+        id,
+      },
     });
 
     if (!answer) {
@@ -30,9 +32,13 @@ export class PrismaAnswersRepository implements AnswersRepository {
     questionId: string,
     { page }: PaginationParams
   ): Promise<Answer[]> {
-    const answers = await this.prismaService.answer.findMany({
-      where: { questionId },
-      orderBy: { createdAt: 'desc' },
+    const answers = await this.prisma.answer.findMany({
+      where: {
+        questionId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
       take: 20,
       skip: (page - 1) * 20,
     });
@@ -43,7 +49,7 @@ export class PrismaAnswersRepository implements AnswersRepository {
   async create(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer);
 
-    await this.prismaService.answer.create({
+    await this.prisma.answer.create({
       data,
     });
 
@@ -57,15 +63,29 @@ export class PrismaAnswersRepository implements AnswersRepository {
   async save(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer);
 
-    await this.prismaService.answer.update({
-      where: { id: data.id },
-      data,
-    });
+    await Promise.all([
+      this.prisma.answer.update({
+        where: {
+          id: answer.id.toString(),
+        },
+        data,
+      }),
+      this.answerAttachmentsRepository.createMany(
+        answer.attachments.getNewItems()
+      ),
+      this.answerAttachmentsRepository.deleteMany(
+        answer.attachments.getRemovedItems()
+      ),
+    ]);
+
+    DomainEvents.dispatchEventsForAggregate(answer.id);
   }
 
   async delete(answer: Answer): Promise<void> {
-    await this.prismaService.answer.delete({
-      where: { id: answer.id.toString() },
+    await this.prisma.answer.delete({
+      where: {
+        id: answer.id.toString(),
+      },
     });
   }
 }
